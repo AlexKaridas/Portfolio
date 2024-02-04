942365271f332270bbe48d5a6db85a994d3e7891
Alex Karidas <karidasalex@gmail.com>
2024-02-04 13:01:35 +0200

    New portfolio project

---
+++ components/Project.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Project = ({ imageSource, imageAlt, title, description }) => {
+  return (
+    <div className="flex items-start mt-24 mb-24">
+      <div className="flex items-start w-2/4">
+        <Image
+          src={imageSource}
+          alt={imageAlt}
+          width={300}
+          height={300}
+          className="ring-1 ring-gray-100 rounded-sm"
+        />
+      </div>
+      <div className="flex flex-col justify-start items-start w-3/4">
+        <h2 className="text-lg font-semibold hover:text-sky-200	transition-all">
+          <Link href="https://www.stonevillamouzaki.online/" target="_blank">
+            {title}
+          </Link>
+        </h2>
+        <p className="text-sm mt-5">{description}</p>
+      </div>
+    </div>
+  );
+};
+export default Project;
